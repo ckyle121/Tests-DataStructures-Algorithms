db665de184f5b87f8befff95a7349cfad30caff1
ckyle121 <carolinekyle21@gmail.com>
2022-12-02 15:32:58 -0600

    solved with set

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,14 +1,15 @@
 import java.util.*;
+import java.util.Set;
 
 class MissingNumber {
-
-
     public static void main(String[] args) {
         int[] array1 = {1,2,3,4,5,6,7,9,10};
         int[] array2 = {2,6,4,1,3,7,8,9};
+        int[] array3 = {1,3,5,7,9,2,4,6,8,10};
 
-        System.out.println(missingNumber(array1, 9));        
-        System.out.println(missingNumber(array2, 10));
+        System.out.println(missingNumberTwo(array1, 9));        
+        System.out.println(missingNumberTwo(array2, 10));
+        System.out.println(missingNumberTwo(array3, 10));
     }
 
     public static int missingNumber(int[] nums, int maxNum){
@@ -19,7 +20,22 @@ class MissingNumber {
                 return i;
             } 
         }
-
         return 0;
+    }
+
+    public static int missingNumberTwo(int[] nums, int maxNum){
+       HashSet set = new HashSet<>();
+
+       for (int j = 0; j < nums.length; j++){
+        set.add(nums[j]);
+       }
+
+        for (int i = 1; i <= maxNum; i++){
+            if (!set.contains(i)){
+                return i; 
+            }
+        }
+        
+        return 0; 
     }
 }
