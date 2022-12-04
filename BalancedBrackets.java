d00f6ab14ce6121d84ddc8f4908252a6440c15b1
ckyle121 <carolinekyle21@gmail.com>
2022-12-04 13:21:58 -0600

    balanced parentheses problem

--- BalancedBrackets.java
+++ BalancedBrackets.java
@@ -10,6 +10,30 @@ public class BalancedBrackets{
         System.out.println(balancedBrackets(stringone));
         System.out.println(balancedBrackets(stringtwo));
         System.out.println(balancedBrackets(stringthree));
+
+
+        String stringfour = "(())";
+        String stringfive = "(())))";
+
+        System.out.println(balancedParentheses(stringfour));
+        System.out.println(balancedParentheses(stringfive));
+    }
+
+    public static boolean balancedParentheses(String s){
+        int numParentheses = 0;
+
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '('){
+                numParentheses++;
+            } else if (s.charAt(i) == ')') {
+                numParentheses--;
+            }
+
+            if (numParentheses  < 0){
+                return false;
+            }
+        }
+        return numParentheses == 0;
     }
 
     public static boolean balancedBrackets(String charString) {
